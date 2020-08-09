5ac9cb941121ce8c6d2b4f7c31d7e62572c7e76f
Deep Bratt Mondal <deepbratt@gmail.com>
2020-08-10 01:43:16 +0530

    user with setup

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const mongoDBErrors = require("mongoose-mongodb-errors");
+const mongoURI = process.env.MONGOURI;
+
+mongoose.Promise = global.Promise;
+mongoose.plugin(mongoDBErrors);
+mongoose.connect(
+  mongoURI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  function (req, res) {}
+);
